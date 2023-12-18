abadd8336e4aa4537b17ed05cb9fea4019e619e6
jhstar2022 <121663730+jhstar2022@users.noreply.github.com>
2023-12-18 21:18:08 +0900

    app_urls 수정

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -19,4 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('user/', include('user.urls')),
+    path('', views.IndexMain.as_view())
 ]
